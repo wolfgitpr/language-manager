c014363ce4440f0d85103033b58a999d69270987
wolfgitpr <133209402+wolfgitpr@users.noreply.github.com>
2024-11-04 08:25:56 +0800

    update trans

--- share/translations/language-manager_zh_CN.ts
+++ share/translations/language-manager_zh_CN.ts
@@ -2,337 +2,297 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
-    <name>LangMgr::Cantonese</name>
+    <name>LangMgr::CantoneseAnalysis</name>
     <message>
-        <location filename="../../src/G2pMgr/G2p/Cantonese.cpp" line="6"/>
+        <location filename="../../src/LangAnalysis/CantoneseAnalysis.h" line="14"/>
+        <source>Cantonese</source>
+        <translation>粤语</translation>
+    </message>
+</context>
+<context>
+    <name>LangMgr::CantoneseG2p</name>
+    <message>
+        <location filename="../../src/G2p/CantoneseG2p.cpp" line="10"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/G2pMgr/G2p/Cantonese.cpp" line="7"/>
+        <location filename="../../src/G2p/CantoneseG2p.cpp" line="11"/>
         <source>Cantonese</source>
         <translation>粤语</translation>
     </message>
     <message>
-        <location filename="../../src/G2pMgr/G2p/Cantonese.cpp" line="8"/>
+        <location filename="../../src/G2p/CantoneseG2p.cpp" line="12"/>
         <source>Using Cantonese Pinyin as the phonetic notation method.</source>
         <translation>使用粤拼注音。</translation>
     </message>
     <message>
-        <location filename="../../src/G2pMgr/G2p/Cantonese.cpp" line="16"/>
+        <location filename="../../src/G2p/CantoneseG2p.cpp" line="22"/>
         <source>Failed to initialize Cantonese G2P</source>
         <translation>加载粤拼G2p失败</translation>
     </message>
 </context>
 <context>
-    <name>LangMgr::CantoneseAnalysis</name>
+    <name>LangMgr::EnglishAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/CantoneseAnalysis.h" line="14"/>
-        <source>Xiao Lang</source>
-        <translation>小狼</translation>
-    </message>
-    <message>
-        <location filename="../../src/LangMgr/LangAnalysis/CantoneseAnalysis.h" line="15"/>
-        <source>Cantonese</source>
-        <translation>粤语</translation>
-    </message>
-    <message>
-        <location filename="../../src/LangMgr/LangAnalysis/CantoneseAnalysis.h" line="16"/>
-        <source>Capture Cantonese characters.</source>
-        <translation>捕获粤语字符。</translation>
+        <location filename="../../src/LangAnalysis/EnglishAnalysis.h" line="13"/>
+        <source>English</source>
+        <translation>英语</translation>
     </message>
 </context>
 <context>
-    <name>LangMgr::English</name>
+    <name>LangMgr::EnglishG2p</name>
     <message>
-        <location filename="../../src/G2pMgr/G2p/English.cpp" line="5"/>
+        <location filename="../../src/G2p/EnglishG2p.cpp" line="9"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/G2pMgr/G2p/English.cpp" line="6"/>
+        <location filename="../../src/G2p/EnglishG2p.cpp" line="10"/>
         <source>English</source>
         <translation>英语</translation>
     </message>
     <message>
-        <location filename="../../src/G2pMgr/G2p/English.cpp" line="7"/>
+        <location filename="../../src/G2p/EnglishG2p.cpp" line="11"/>
         <source>Greedy matching of consecutive English letters.</source>
         <translation>贪婪匹配英文字母。</translation>
     </message>
 </context>
 <context>
-    <name>LangMgr::EnglishAnalysis</name>
+    <name>LangMgr::JapaneseG2p</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/EnglishAnalysis.h" line="13"/>
+        <location filename="../../src/G2p/JapaneseG2p.cpp" line="12"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/EnglishAnalysis.h" line="14"/>
-        <source>English</source>
-        <translation>英语</translation>
+        <location filename="../../src/G2p/JapaneseG2p.cpp" line="13"/>
+        <source>Japanese</source>
+        <translation>日语</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/EnglishAnalysis.h" line="15"/>
-        <source>Capture English words.</source>
-        <translation>捕获英语单词。</translation>
+        <location filename="../../src/G2p/JapaneseG2p.cpp" line="14"/>
+        <source>Kana to Romaji converter.</source>
+        <translation>假名-罗马音转换器。</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::JyutpingAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/JyutpingAnalysis.h" line="17"/>
-        <source>Xiao Lang</source>
-        <translation>小狼</translation>
-    </message>
-    <message>
-        <location filename="../../src/LangMgr/LangAnalysis/JyutpingAnalysis.h" line="18"/>
+        <location filename="../../src/LangAnalysis/JyutpingAnalysis.h" line="18"/>
         <source>Jyutping</source>
         <translation>粤拼</translation>
-    </message>
-    <message>
-        <location filename="../../src/LangMgr/LangAnalysis/JyutpingAnalysis.h" line="19"/>
-        <source>Capture Jyutping words.</source>
-        <translation>捕获粤语拼音。</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::KanaAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/KanaAnalysis.h" line="13"/>
-        <source>Xiao Lang</source>
-        <translation>小狼</translation>
-    </message>
-    <message>
-        <location filename="../../src/LangMgr/LangAnalysis/KanaAnalysis.h" line="14"/>
+        <location filename="../../src/LangAnalysis/KanaAnalysis.h" line="14"/>
         <source>Kana</source>
         <translation>假名</translation>
-    </message>
-    <message>
-        <location filename="../../src/LangMgr/LangAnalysis/KanaAnalysis.h" line="16"/>
-        <source>Capture Kana characters.</source>
-        <translation>捕获假名字符。</translation>
-    </message>
-</context>
-<context>
-    <name>LangMgr::KanaG2p</name>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Kana.cpp" line="7"/>
-        <source>Xiao Lang</source>
-        <translation>小狼</translation>
-    </message>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Kana.cpp" line="8"/>
-        <source>Kana</source>
-        <translation>假名</translation>
-    </message>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Kana.cpp" line="9"/>
-        <source>Kana to Romanization converter.</source>
-        <translation>假名-罗马音转换器。</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::LinebreakAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/LinebreakAnalysis.h" line="14"/>
+        <location filename="../../src/LangAnalysis/BaseAnalysis/LinebreakAnalysis.h" line="13"/>
+        <source>Linebreak</source>
+        <translation>换行符</translation>
+    </message>
+</context>
+<context>
+    <name>LangMgr::LinebreakG2p</name>
+    <message>
+        <location filename="../../src/G2p/BaseG2p/LinebreakG2p.cpp" line="9"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/LinebreakAnalysis.h" line="15"/>
+        <location filename="../../src/G2p/BaseG2p/LinebreakG2p.cpp" line="10"/>
         <source>Linebreak</source>
         <translation>换行符</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/LinebreakAnalysis.h" line="16"/>
-        <source>Capture linebreaks.</source>
-        <translation>捕获换行符。</translation>
-    </message>
-</context>
-<context>
-    <name>LangMgr::Mandarin</name>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Mandarin.cpp" line="6"/>
-        <source>Xiao Lang</source>
-        <translation>小狼</translation>
-    </message>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Mandarin.cpp" line="7"/>
-        <source>Mandarin</source>
-        <translation>普通话</translation>
-    </message>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Mandarin.cpp" line="8"/>
-        <source>Using Pinyin as the phonetic notation method.</source>
-        <translation>使用拼音注音。</translation>
-    </message>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Mandarin.cpp" line="16"/>
-        <source>Failed to initialize Mandarin G2P</source>
-        <translation>加载拼音G2p失败</translation>
+        <location filename="../../src/G2p/BaseG2p/LinebreakG2p.cpp" line="11"/>
+        <source>Linebreak, no conversion required.</source>
+        <translation>换行符，无需转换。</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::MandarinAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/MandarinAnalysis.h" line="13"/>
+        <location filename="../../src/LangAnalysis/MandarinAnalysis.h" line="13"/>
+        <source>Mandarin</source>
+        <translation>普通话</translation>
+    </message>
+</context>
+<context>
+    <name>LangMgr::MandarinG2p</name>
+    <message>
+        <location filename="../../src/G2p/MandarinG2p.cpp" line="10"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/MandarinAnalysis.h" line="14"/>
+        <location filename="../../src/G2p/MandarinG2p.cpp" line="11"/>
         <source>Mandarin</source>
         <translation>普通话</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/MandarinAnalysis.h" line="15"/>
-        <source>Capture Mandarin characters.</source>
-        <translation>捕获中文字符。</translation>
+        <location filename="../../src/G2p/MandarinG2p.cpp" line="12"/>
+        <source>Using Pinyin as the phonetic notation method.</source>
+        <translation>使用拼音注音。</translation>
+    </message>
+    <message>
+        <location filename="../../src/G2p/MandarinG2p.cpp" line="22"/>
+        <source>Failed to initialize Mandarin G2P</source>
+        <translation>加载拼音G2p失败</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::NumberAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/NumberAnalysis.h" line="13"/>
+        <location filename="../../src/LangAnalysis/BaseAnalysis/NumberAnalysis.h" line="13"/>
+        <source>Number</source>
+        <translation>数字</translation>
+    </message>
+</context>
+<context>
+    <name>LangMgr::NumberG2p</name>
+    <message>
+        <location filename="../../src/G2p/BaseG2p/NumberG2p.cpp" line="9"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/NumberAnalysis.h" line="14"/>
+        <location filename="../../src/G2p/BaseG2p/NumberG2p.cpp" line="10"/>
         <source>Number</source>
         <translation>数字</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/NumberAnalysis.h" line="15"/>
-        <source>Capture numbers.</source>
-        <translation>捕获数字。</translation>
+        <location filename="../../src/G2p/BaseG2p/NumberG2p.cpp" line="11"/>
+        <source>Number, no conversion required.</source>
+        <translation>数字，无需转换。</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::PinyinAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/PinyinAnalysis.h" line="16"/>
-        <source>Xiao Lang</source>
-        <translation>小狼</translation>
-    </message>
-    <message>
-        <location filename="../../src/LangMgr/LangAnalysis/PinyinAnalysis.h" line="17"/>
+        <location filename="../../src/LangAnalysis/PinyinAnalysis.h" line="17"/>
         <source>Pinyin</source>
         <translation>拼音</translation>
     </message>
+</context>
+<context>
+    <name>LangMgr::PunctuationAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/PinyinAnalysis.h" line="18"/>
-        <source>Capture Pinyin words.</source>
-        <translation>捕获拼音。</translation>
+        <location filename="../../src/LangAnalysis/BaseAnalysis/PunctuationAnalysis.h" line="14"/>
+        <source>Punctuation</source>
+        <translation>标点</translation>
     </message>
 </context>
 <context>
-    <name>LangMgr::Punctuation</name>
+    <name>LangMgr::PunctuationG2p</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/Punctuation.h" line="14"/>
+        <location filename="../../src/G2p/BaseG2p/PunctuationG2p.cpp" line="9"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/Punctuation.h" line="15"/>
+        <location filename="../../src/G2p/BaseG2p/PunctuationG2p.cpp" line="10"/>
         <source>Punctuation</source>
         <translation>标点</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/Punctuation.h" line="16"/>
-        <source>Capture punctuations.</source>
-        <translation>捕获标点。</translation>
+        <location filename="../../src/G2p/BaseG2p/PunctuationG2p.cpp" line="11"/>
+        <source>Punctuation, no conversion required.</source>
+        <translation>标点，无需转换。</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::RomajiAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/RomajiAnalysis.h" line="16"/>
-        <source>Xiao Lang</source>
-        <translation>小狼</translation>
-    </message>
-    <message>
-        <location filename="../../src/LangMgr/LangAnalysis/RomajiAnalysis.h" line="17"/>
+        <location filename="../../src/LangAnalysis/RomajiAnalysis.h" line="17"/>
         <source>Romaji</source>
         <translation>罗马音</translation>
-    </message>
-    <message>
-        <location filename="../../src/LangMgr/LangAnalysis/RomajiAnalysis.h" line="18"/>
-        <source>Capture Romaji words.</source>
-        <translation>捕获罗马音。</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::SlurAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/SlurAnalysis.h" line="14"/>
+        <location filename="../../src/LangAnalysis/BaseAnalysis/SlurAnalysis.h" line="14"/>
+        <source>Slur</source>
+        <translation>延音符</translation>
+    </message>
+</context>
+<context>
+    <name>LangMgr::SlurG2p</name>
+    <message>
+        <location filename="../../src/G2p/BaseG2p/SlurG2p.cpp" line="8"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/SlurAnalysis.h" line="15"/>
+        <location filename="../../src/G2p/BaseG2p/SlurG2p.cpp" line="9"/>
         <source>Slur</source>
         <translation>延音符</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/SlurAnalysis.h" line="16"/>
-        <source>Capture slurs.</source>
-        <translation>捕获延音符。</translation>
+        <location filename="../../src/G2p/BaseG2p/SlurG2p.cpp" line="10"/>
+        <source>Slur, no conversion required.</source>
+        <translation>延音符，无需转换。</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::SpaceAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/SpaceAnalysis.h" line="14"/>
+        <location filename="../../src/LangAnalysis/BaseAnalysis/SpaceAnalysis.h" line="14"/>
+        <source>Space</source>
+        <translation>空格</translation>
+    </message>
+</context>
+<context>
+    <name>LangMgr::SpaceG2p</name>
+    <message>
+        <location filename="../../src/G2p/BaseG2p/SpaceG2p.cpp" line="8"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/SpaceAnalysis.h" line="15"/>
+        <location filename="../../src/G2p/BaseG2p/SpaceG2p.cpp" line="9"/>
         <source>Space</source>
         <translation>空格</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/SpaceAnalysis.h" line="16"/>
-        <source>Capture spaces.</source>
-        <translation>捕获空格。</translation>
-    </message>
-</context>
-<context>
-    <name>LangMgr::Unknown</name>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Unknown.cpp" line="5"/>
-        <source>Xiao Lang</source>
-        <translation>小狼</translation>
-    </message>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Unknown.cpp" line="6"/>
-        <source>Unknown</source>
-        <translation>未知</translation>
-    </message>
-    <message>
-        <location filename="../../src/G2pMgr/G2p/Unknown.cpp" line="7"/>
-        <source>Unknown language, no conversion required.</source>
-        <translation>未知语言，无需转换。</translation>
+        <location filename="../../src/G2p/BaseG2p/SpaceG2p.cpp" line="10"/>
+        <source>Space, no conversion required.</source>
+        <translation>空格，无需转换。</translation>
     </message>
 </context>
 <context>
     <name>LangMgr::UnknownAnalysis</name>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/UnknownAnalysis.h" line="13"/>
+        <location filename="../../src/LangAnalysis/BaseAnalysis/UnknownAnalysis.h" line="14"/>
+        <source>Unknown</source>
+        <translation>未知</translation>
+    </message>
+</context>
+<context>
+    <name>LangMgr::UnknownG2p</name>
+    <message>
+        <location filename="../../src/G2p/BaseG2p/UnknownG2p.cpp" line="9"/>
         <source>Xiao Lang</source>
         <translation>小狼</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/UnknownAnalysis.h" line="14"/>
+        <location filename="../../src/G2p/BaseG2p/UnknownG2p.cpp" line="10"/>
         <source>Unknown</source>
         <translation>未知</translation>
     </message>
     <message>
-        <location filename="../../src/LangMgr/LangAnalysis/BaseAnalysis/UnknownAnalysis.h" line="15"/>
-        <source>Capture unknown characters.</source>
-        <translation>捕获未知字符。</translation>
+        <location filename="../../src/G2p/BaseG2p/UnknownG2p.cpp" line="11"/>
+        <source>Unknown language, no conversion required.</source>
+        <translation>未知语言，无需转换。</translation>
     </message>
 </context>
 </TS>
